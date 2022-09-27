6ebf2be81017d4a44ecb24628afdd3d1516e967a
PierreEVEN <evenpierre@orange.fr>
2022-09-27 10:05:10 +0200

    wip terrain renderer v2

--- src/geometry/planet.h
+++ src/geometry/planet.h
@@ -11,7 +11,6 @@ class PlanetRegion;
 class Planet : public SceneComponent
 {
 public:
-
 	Planet();
 	virtual ~Planet() override;
 
@@ -20,33 +19,20 @@ protected:
 	void render() override;
 
 private:
-	std::vector<PlanetRegion> initial_regions;
-
-	std::shared_ptr<Mesh> final_mesh;
-	std::shared_ptr<Material> planet_material;
-
-	void regenerate();
-
-	int subdivisions = 1;
-	std::atomic_bool is_waiting_regeneration = false;
-	std::atomic_bool regeneration_job_done = false;
-	std::thread regeneration_thread;
 };
 
 class PlanetRegion
 {
 public:
-	PlanetRegion(const Eigen::Vector3d& a, const Eigen::Vector3d& b, const Eigen::Vector3d& c);
+	PlanetRegion(int subdivision_level, double width, double inner_radius);
 
-	[[nodiscard]] std::vector<Eigen::Vector3d> collect_triangles() const;
-
-	void subdivide(int sub_index);
+	void render() const;
 
 private:
 
-	PlanetRegion* parent = nullptr;
-	std::vector<PlanetRegion*> neighbors;
-	std::vector<PlanetRegion> children;
+	int subdivision_level;
 
-	Eigen::Vector3d a, b, c;
+	std::shared_ptr<Mesh> mesh;
+	std::shared_ptr<Material> material;
+	std::shared_ptr<PlanetRegion> child;
 };
